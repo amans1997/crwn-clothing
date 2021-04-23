11c78e3846fe331693a9916ba640caf8bda238dc
aman101097 <aman.sharma@bankbuddy.ai>
2021-04-23 19:36:52 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -6,39 +6,40 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component.jsx';
 import  SignInAndSignUpPage  from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth,createUserProfileDocument} from './firebase/firebase.utils'
+import {connect } from 'react-redux';
 
+import {setCurrentUser} from  './redux/user/user.action';
 
 class  App extends React.Component{
   
 
   constructor(){
     super();
-
-    this.state={
-      currentUser:null
-    };
   }
+   
 
   componentDidMount() {
+
+    const {setCurrentUser}=this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          this.props.setCurrentUser({
+            
               id: snapShot.id,
               ...snapShot.data()
-            }
+            
           });
 
           console.log(this.state);
         });
       }
 
-      this.setState({ currentUser: userAuth });
+     setCurrentUser( userAuth );
     });
-  }
+  };
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -50,7 +51,7 @@ class  App extends React.Component{
   render(){
     return (
     <div >
-      <Header currentUser={this.state.currentUser}/>
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
@@ -60,4 +61,9 @@ class  App extends React.Component{
   );}
 }
 
-export default App;
+
+const mapDispatchtoProps= dispatch=>({
+  setCurrentUser:user=>dispatch(setCurrentUser(user))
+});
+
+export default connect(null,mapDispatchtoProps)(App);
---
+++ src/redux/user/user.reducer.jsx
@@ -0,0 +1,22 @@
+//Reducer gets a state object and it receives an action
+
+
+const INITIAL_STATE={
+    currentUser:null
+}
+
+const userReducer=(state=INITIAL_STATE,action)=>{
+    switch(action.type){
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser:action.payload
+            }
+
+
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
